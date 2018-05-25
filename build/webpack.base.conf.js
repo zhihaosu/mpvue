29f6ce24a8418bb05876a4de189f38f90b2bc0ad
zhihaosu <zhihao_su@intsig.net>
2018-05-25 14:17:03 +0800

    .

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -102,29 +102,10 @@ module.exports = {
       {
         test: /\.scss$/,
         include: [resolve('src'), resolve('test')],
-        use: [
-          {
-            loader: 'vue-style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: 'true'
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
+        loader: 'sass-loader',
+        options: {
+          sourceMap: true
+        }
       },
     ]
   },
